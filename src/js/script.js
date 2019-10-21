6b3be7f9835ef548f1862e4a1ea7bae692b3c830
Arsen <baregamyan@gmail.com>
2019-10-21 15:58:56 +0300

    Оживляет бургер в навигации

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,24 @@
 'use strict';
-var slider = new Swiper('.header__sliders', {
+/**
+ * Меню в навигации сайта
+ */
+const menus = document.querySelectorAll('.menu');
+const burger = document.querySelector('.nav__toggle');
+
+/**
+ * Скрытие/открытие меню навигации сайта в мобильной и планшетной версии
+ */
+burger.addEventListener('click', (evt)=> {
+  console.log('Click on burger');
+  menus.forEach(menu=> {
+    menu.classList.toggle('menu--open');
+  })
+});
+
+/**
+ * Инициализация слайдера в шапке
+ */
+const slider = new Swiper('.header__sliders', {
   loop: true,
   spaceBetween: 150,
   pagination: {
