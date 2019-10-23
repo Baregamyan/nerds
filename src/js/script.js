3a626c62f9e1415297adc221a4f225bcda5aa0b3
Arsen <baregamyan@gmail.com>
2019-10-23 13:54:31 +0300

    Добавляет активный пункт меню и проверку отсутсвия js для меню навигации в мобильной версии

--- src/js/script.js
+++ src/js/script.js
@@ -6,6 +6,13 @@ const menus = document.querySelectorAll('.menu');
 const burger = document.querySelector('.nav__toggle');
 
 /**
+ * Скрытие всех меню в мобильной версии, если js не подгрузился
+ */
+menus.forEach( menu=> {
+  menu.classList.remove('menu--no-js');
+});
+
+/**
  * Скрытие/открытие меню навигации сайта в мобильной и планшетной версии
  */
 burger.addEventListener('click', (evt)=> {
