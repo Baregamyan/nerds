a317a678bafa063056a617e78e189e6ce1e04a57
baregamyan <baregamyan@gmail.com>
2021-12-21 05:57:32 +0300

    update

--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -3,20 +3,31 @@
  */
 const menus = document.querySelectorAll('.menu');
 const burger = document.querySelector('.nav__toggle');
+let isOpen = true;
 
 /**
  * Скрытие всех меню в мобильной версии, если js не подгрузился
  */
 menus.forEach( menu=> {
   menu.classList.remove('menu--no-js');
+  isOpen = false;
+  burger.setAttribute('aria-label', 'Открыть меню навигации');
 });
 
 /**
  * Скрытие/открытие меню навигации сайта в мобильной и планшетной версии
  */
 burger.addEventListener('click', (evt)=> {
-  console.log('Click on burger');
   menus.forEach(menu=> {
     menu.classList.toggle('menu--open');
+    if (isOpen) {
+      console.log('Opened')
+      isOpen = false;
+      burger.setAttribute('aria-label', 'Закрыть меню навигации');
+    } else {
+      console.log('Closed')
+      isOpen = true;
+      burger.setAttribute('aria-label', 'Открыть меню навигации');
+    }
   })
 });
